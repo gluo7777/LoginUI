fc412be41c6ee4b97e7a49af2c883bcc4f019de3
William Luo <gluo7777@gmail.com>
2019-10-09 20:27:56 -0500

    added spring-boot boiler plate

---
+++ spring-react-session/src/main/java/org/gluo7777/springreactsession/SpringReactSessionApplication.java
@@ -0,0 +1,13 @@
+package org.gluo7777.springreactsession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactSessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactSessionApplication.class, args);
+	}
+
+}
