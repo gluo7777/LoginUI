b60521d9d4a0087de399919d9d93c790a7c36bca
William Luo <gluo7777@gmail.com>
2019-12-13 17:30:54 -0600

    added snackbar

--- LoginUI/src/registration/ParentForm.js
+++ LoginUI/src/registration/ParentForm.js
@@ -1,5 +1,6 @@
-import { Button, Container, Grid, Step, StepLabel, Stepper, Typography } from '@material-ui/core';
+import { Button, Container, Grid, Step, StepLabel, Stepper, Typography, Snackbar, SnackbarContent, IconButton } from '@material-ui/core';
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import { Redirect, Route } from 'react-router-dom';
 import * as Client from '../http/Client';
 import { AccountForm } from './AccountForm';
@@ -7,6 +8,10 @@ import { buildAccountInfoRequest } from './FormComponents';
 import { PersonalForm } from './PersonalForm';
 import { RegistrationContext, useStyles } from './Registration';
 import { ReviewForm } from './ReviewForm';
+import { makeStyles } from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
+import ErrorIcon from '@material-ui/icons/Error';
+
 export function ParentForm() {
     const classes = useStyles();
     const { data } = React.useContext(RegistrationContext);
@@ -14,6 +19,10 @@ export function ParentForm() {
     const [action, setAction] = useState('register');
     const [errors, setErrors] = React.useState({});
     const [errorTexts, setErrorTexts] = React.useState({});
+    const [alert, setAlert] = React.useState({
+        open: false,
+        message: ""
+    });
     const validationMethods = {
         hasError: (field) => {
             let error = errors[field];
@@ -66,37 +75,122 @@ export function ParentForm() {
             setStep(newStep);
         }
         else if (newStep === forms.length) {
+            // check and notify of errors
+            for (let field in errors) {
+                if (errors[field]) {
+                    setAlert({
+                        ...alert,
+                        open: true,
+                        message: "There are errors in the form."
+                    });
+                    return;
+                }
+            }
+            // Call API to register user
             const accountInfo = buildAccountInfoRequest(data);
             const success = await Client.registerAccount(accountInfo);
             if (success) {
                 // redirect to login page
-                console.log("Succesfully registered user!");
                 setAction('login');
             }
             else {
                 // show error pop up
-                console.log("Failed to register!");
-                setAction('error');
+                setAlert({
+                    ...alert,
+                    open: true,
+                    message: "Failed to register account. Please try again later."
+                });
+                return;
             }
         }
     };
-    return (<Container component="main" maxWidth="md" className={classes.container}>
-        <Typography variant="h5" component="h1" className={classes.heading}>Registration</Typography>
-        <Container>
-            <Stepper activeStep={step} className={classes.stepper}>
-                {forms.map(form => <Step key={form.label} completed={false}>
-                    <StepLabel>{form.label}</StepLabel>
-                </Step>)}
-            </Stepper>
-            {forms[step].form}
-            <Grid container spacing={1} item sm={12} justify="space-between">
-                <Grid item sm={6} container justify="flex-start">
-                    <Button color="secondary" variant="contained" onClick={handleStepChange(-1)}>Back</Button>
+    return (
+        <Container component="main" maxWidth="md" className={classes.container}>
+            <Typography variant="h5" component="h1" className={classes.heading}>Registration</Typography>
+            <Container>
+                <Stepper activeStep={step} className={classes.stepper}>
+                    {forms.map(form => <Step key={form.label} completed={false}>
+                        <StepLabel>{form.label}</StepLabel>
+                    </Step>)}
+                </Stepper>
+                <ErrorSnackBar open={alert.open} message={alert.message} onClose={() => setAlert({ ...alert, open: false })} />
+                {forms[step].form}
+                <Grid container spacing={1} item sm={12} justify="space-between">
+                    <Grid item sm={6} container justify="flex-start">
+                        <Button color="secondary" variant="contained" onClick={handleStepChange(-1)}>Back</Button>
+                    </Grid>
+                    <Grid item sm={6} container justify="flex-end">
+                        <Button color="primary" variant="contained" onClick={handleStepChange(1)}>{step === forms.length - 1 ? "Finish" : "Next"}</Button>
+                    </Grid>
                 </Grid>
-                <Grid item sm={6} container justify="flex-end">
-                    <Button color="primary" variant="contained" onClick={handleStepChange(1)}>{step === forms.length - 1 ? "Finish" : "Next"}</Button>
-                </Grid>
-            </Grid>
+            </Container>
         </Container>
-    </Container>);
+    );
 }
+
+const snackBarStyles = makeStyles(theme => ({
+    close: {
+        padding: theme.spacing(0.5),
+    },
+    error: {
+        backgroundColor: theme.palette.error.dark,
+    },
+    message: {
+        display: 'flex',
+        alignItems: 'center',
+    },
+    icon: {
+        fontSize: 20,
+        opacity: 0.9,
+        marginRight: theme.spacing(1)
+    }
+}));
+
+export default function ErrorSnackBar(props) {
+    const { message, open, onClose } = props;
+    const classes = snackBarStyles();
+
+    return (
+        <Snackbar
+            anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'center',
+            }}
+            open={open}
+            autoHideDuration={6000}
+            onClose={onClose}
+            ContentProps={{
+                'aria-describedby': 'message-id',
+            }}
+        >
+            <SnackbarContent
+                className={classes.error}
+                message={
+                    <span id="message-id" className={classes.message}>
+                        <ErrorIcon className={classes.icon} />
+                        {message}
+                    </span>
+                }
+                action={[
+                    //         <Button key="undo" color="primary" size="small" onClick={onClose}>
+                    //             HELP
+                    // </Button>,
+                    <IconButton
+                        key="close"
+                        aria-label="close"
+                        color="inherit"
+                        className={classes.close}
+                        onClick={onClose}
+                    >
+                        <CloseIcon />
+                    </IconButton>,
+                ]}
+            />
+        </Snackbar>
+    );
+}
+
+ErrorSnackBar.propTypes = {
+    open: PropTypes.bool,
+    onClose: PropTypes.func
+};
